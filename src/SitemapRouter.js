6b953929b509c9a59c321a05a059c901897ded0a
hoseol OH <dhgh9590@naver.com>
2022-12-11 21:30:55 +0900

    SEO

---
+++ src/SitemapRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+
+export default (
+  <Switch>
+    <Route path="/" />
+    <Route path="/about" />
+    <Route path="/mywork" />
+    <Route path="/contact" />
+    <Route />
+  </Switch>
+);
